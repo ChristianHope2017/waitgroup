b06eab8bd5b8efba5236855e94dc4994cef3dfba
Christian Hope <2017117171@ub.edu.bz>
2023-02-13 13:12:52 -0600

    Wait Groups

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ChristianHope2017/wait-group
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,29 @@
+// Filename: main.go
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func one (wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("hola")
+}
+func two(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("ni hao")
+}
+func three (wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("hello")
+}
+func main(){
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go one(&wg)
+	go two(&wg)
+	go three(&wg)
+	//Let's Delay
+	wg.Wait()
+}
